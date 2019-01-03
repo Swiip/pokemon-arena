1364f44b794a0c314ecea906fd41118c32b34e91
Swiip <matthieu.lux@gmail.com>
2019-01-03 11:52:23 +0100

    update scrapper

--- api/scrapper.js
+++ api/scrapper.js
@@ -1,53 +1,76 @@
-const axios = require('axios');
+const path = require("path");
+const fs = require("fs-extra");
+const axios = require("axios");
+const { mapSeries } = require("bluebird");
+
 const dataAPI = `https://pokeapi.co/api/v2/pokemon`;
 const gifAPI = `http://www.pokestadium.com/sprites/xy/`;
 
-const path = require('path');
-const util = require('util');
-const fs = require('fs');
-const writeFile = util.promisify(fs.writeFile);
-const exists = util.promisify(fs.exists);
-const mkdir = util.promisify(fs.mkdir);
+const getGif = async gifPath => {
+  const options = {
+    headers: { Accept: "image/gif" },
+    responseType: "stream"
+  };
 
-async function ensureDir(path) {
-  const exist = await exists(path);
+  try {
+    const response = await axios.get(`${gifAPI}${gifPath}.gif`, options);
 
-  if(!exist) {
-    console.log(`Creating folder ${path}`);
-    return mkdir(path);
+    await response.data.pipe(
+      fs.createWriteStream(path.join(__dirname, `./data/img/${gifPath}.gif`))
+    );
+
+    console.log(`${gifPath} gif scrapped`);
+  } catch (error) {
+    console.log(`${gifPath} gif NOT scrapped, DO NOT USE THIS POKEMON`);
+    throw error;
   }
-}
+};
+
+const getData = async pokemonName => {
+  try {
+    const response = await axios.get(`${dataAPI}/${pokemonName}`);
+
+    await fs.writeFile(
+      path.join(__dirname, `./data/pokemon/${pokemonName}.json`),
+      JSON.stringify(response.data)
+    );
+
+    console.log(`${pokemonName} data scrapped`);
+  } catch (error) {
+    console.log(`${pokemonName} data NOT scrapped, DO NOT USE THIS POKEMON`);
+    throw error;
+  }
+};
 
 async function scrap() {
   // Create necessary files
-  await ensureDir(path.join(__dirname, './data'));
-  await ensureDir(path.join(__dirname, './data/img'));
+  await fs.ensureDir(path.join(__dirname, "./data"));
+  await fs.ensureDir(path.join(__dirname, "./data/img"));
+  await fs.ensureDir(path.join(__dirname, "./data/img/back"));
+  await fs.ensureDir(path.join(__dirname, "./data/pokemon"));
 
   // get and write pokemons list
   const resPokemon = await axios.get(dataAPI);
 
-  const pokemons = resPokemon.data.results;
+  const pokemons = await mapSeries(resPokemon.data.results, async pokemon => {
+    let ok = true;
+    try {
+      await getGif(pokemon.name);
+      await getGif(`back/${pokemon.name}`);
+      await getData(pokemon.name);
+    } catch (error) {
+      ok = false;
+    } finally {
+      return { ...pokemon, ok };
+    }
+  }).filter(pokemon => pokemon.ok);
 
-  await writeFile(
-    path.join(__dirname, './data/pokemons.json'),
+  await fs.writeFile(
+    path.join(__dirname, "./data/pokemons.json"),
     JSON.stringify(pokemons)
   );
-
-  for(let pokemon of pokemons) {
-    await axios.get(`${gifAPI}${pokemon.name}.gif`, {
-      headers: {
-        Accept: 'image/gif',
-      },
-      responseType:'stream'
-    })
-      .then((response) => response.data
-        .pipe(fs.createWriteStream(path.join(__dirname, `./data/img/${pokemon.name}.gif`)))
-      )
-      .then(() => console.log(`${pokemon.name} gif scrapped`))
-      .catch(() => console.log(`${pokemon.name} gif NOT scrapped, DO NOT USE THIS POKEMON`));
-  }
 }
 
 scrap()
-  .then(() => console.log('Job finished !'))
-  .catch((err) => console.error(err));
+  .then(() => console.log("Job finished !"))
+  .catch(err => console.error(err));
